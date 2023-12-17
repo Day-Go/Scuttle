edc607251f6b5bbcee413a0675f6233f6261612b
Day-Go <lucarusso.521@gmail.com>
2023-12-17 21:31:54 +1100

    following sebastian lague video

---
+++ src/bin.rs
@@ -0,0 +1,22 @@
+// #[derive(Component)]
+// pub enum Charge {
+//     Positive,
+//     Negative,
+//     Neutral
+// }
+
+// #[derive(Bundle)]
+// pub struct ParticleState  {
+//     pub charge: Charge
+
+// }
+
+// impl ParticleState {
+//     pub fn new(charge: Charge) -> Self {
+//         ParticleState {
+//             charge
+//         }
+//     }
+// }
+
+// .insert(ParticleState::new(Charge::Negative))
